66cca72448b33d840e56e8e6f68ea7be9646d166
Shannon Quinn <shannon.quinn91@gmail.com>
2021-03-04 21:33:19 -0500

    Testing API

--- models/User.js
+++ models/User.js
@@ -5,7 +5,6 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
     name: {type: String, required: true},
     email: {type: String, required: true},
-    password: {type: String, required: true},
     watchedMovies: [
         {
            type: Schema.Types.ObjectId,
