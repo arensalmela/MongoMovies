a9a0a835be7298ae29f53f00003d257595cb41cb
Aren S <adsalmela@gmail.com>
2021-03-04 20:07:40 -0500

    Added Login and Logout hooks with Google Authentication.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
+import React from 'react';
 import './App.css';
+import LoginHooks from './components/Login/LoginHooks'
+import LogoutHooks from './components/Logout/LogoutHooks'
 
 function App() {
   return (
     <div className="App">
-
+      <LoginHooks />
+      <LogoutHooks />
     </div>
   );
 }
