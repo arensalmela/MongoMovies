4d0559f32ac310c4ad99f2416437016222669d1f
Junko Yamazaki <junkoo.yamazaki@gmail.com>
2021-03-10 01:00:27 -0500

    Fixed the placeholder cards for trending collections page. Still need to work on CSS on cards and search bar

--- client/src/pages/Collections/Collections.js
+++ client/src/pages/Collections/Collections.js
@@ -1,6 +1,6 @@
 import React from "react";
-import API from "../../utils/API";
-import Search from "../../components/Search";
+// import API from "../../utils/API";
+// import Search from "../../components/Search";
 import CardGrid from '../../components/CardGrid'
 
 export default function Collections({ user }) {
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,21 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import API from '../../utils/API';
+import React from "react";
+// import API from "../../utils/API";
 
-function Home({ user }) {
-    // const [trending, setTrending] = useState();
+export default function Home({ user }) {
+  // const [trending, setTrending] = useState();
 
-    // //When user logs in, show them trending movies
-    // useEffect(() => {
-    //     API.trending()
-    //         .then((response => setTrending(response)))
-    // })
+  // //When user logs in, show them trending movies
+  // useEffect(() => {
+  //     API.trending()
+  //         .then((response => setTrending(response)))
+  // })
 
-
-    return <>
-        {/* <div>{JSON.stringify(trending)}</div> */}
-        <h1>Home!</h1>
-
+  return (
+    <>
+      {/* <div>{JSON.stringify(trending)}</div> */}
+      <h1>Home!</h1>
     </>
-}
-
-export default Home;
+  );
+};
