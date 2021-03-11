05fd5b67c5b07b44d07584a7231866fcd0a33beb
JES20 <dore.joel.dore@gmail.com>
2021-03-11 18:17:48 -0500

    Render trending cards on Homepage

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,19 +1,13 @@
 import React from "react";
+import CardGrid from "../../components/CardGrid";
 // import API from "../../utils/API";
 
 export default function Home({ user }) {
-  // const [trending, setTrending] = useState();
-
-  // //When user logs in, show them trending movies
-  // useEffect(() => {
-  //     API.trending()
-  //         .then((response => setTrending(response)))
-  // })
 
   return (
     <>
-      {/* <div>{JSON.stringify(trending)}</div> */}
       <h1>Home!</h1>
+      <CardGrid />
     </>
   );
 };
