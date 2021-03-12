443c90ebb5cb2a95c62e9fbffb49863468e4465c
JES20 <dore.joel.dore@gmail.com>
2021-03-11 19:47:49 -0500

    Conditionally render '+' icon

--- client/src/components/TrendingCards/index.js
+++ client/src/components/TrendingCards/index.js
@@ -1,37 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
-import CardMedia from "@material-ui/core/CardMedia";
-import CardActions from "@material-ui/core/CardActions";
-import IconButton from "@material-ui/core/IconButton";
-import FavoriteIcon from "@material-ui/icons/Favorite";
-import AddIcon from "@material-ui/icons/Add";
-import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
+import Container from "@material-ui/core/Container";
 import API from '../../utils/API';
-import { CardContent } from "@material-ui/core";
+import MovieCard from "../MovieCard";
 // import logoRed from "../../assets/images/logo-red.svg"
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 345
-  },
-  media: {
-    height: "300px",
-    paddingTop: "56.25%" // 16:9
-  },
-  expand: {
-    transform: "rotate(0deg)",
-    marginLeft: "auto",
-    transition: theme.transitions.create("transform", {
-      duration: theme.transitions.duration.shortest
-    })
-  }
-}));
-
 export default function TrendingCards() {
-  const classes = useStyles();
   const [trending, setTrending] = useState([]);
 
   useEffect(() => {
@@ -39,61 +13,16 @@ export default function TrendingCards() {
       .then(({ data }) => setTrending(data.results));
   }, [])
 
-
   return (
     <>
       {/* Fix the formatting of the cards */}
       <Container>
         <Grid container margin={2} spacing={3}>
           {
-            trending.length && trending.map(movie => {
-              return (
-                <Grid item xs={6} key={movie.id}>
-                  <Card className={classes.root}>
-                    <CardHeader title={movie.title} subheader={movie.release_date} />
-                    <CardMedia
-                      className={classes.media}
-                      image={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-                      title="Movie poster"
-                    />
-                    <CardContent>
-                      <details>
-                        <summary>Overview</summary>
-                        {movie.overview}
-                      </details>
-                    </CardContent>
-                    <CardActions disableSpacing>
-                      <IconButton aria-label="add to favorites">
-                        <FavoriteIcon />
-                      </IconButton>
-                      <IconButton aria-label="share">
-                        <AddIcon />
-                      </IconButton>
-                    </CardActions>
-                  </Card>
-                </Grid>
-              )
-            })
+            trending.length && trending.map(movie => (
+              <MovieCard movie={movie} key={movie.id} />
+            ))
           }
-
-          {/* <Grid item xs={6}>
-            <Card className={classes.root}>
-              <CardHeader title="Title Here?" subheader="Released: XXDATEXX" />
-              <CardMedia
-                className={classes.media}
-                image={logoRed}
-                title="Paella dish"
-              />
-              <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                  <FavoriteIcon />
-                </IconButton>
-                <IconButton aria-label="share">
-                  <AddIcon />
-                </IconButton>
-              </CardActions>
-            </Card>
-          </Grid> */}
         </Grid>
       </Container>
     </>
