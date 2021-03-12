673f637e405deb2ed5ffd2c19a30a73aa13d90f6
JES20 <dore.joel.dore@gmail.com>
2021-03-12 12:09:16 -0500

    Simplify searchbar & save value in state

--- client/src/components/Search/index.js
+++ client/src/components/Search/index.js
@@ -1,72 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { fade, makeStyles } from "@material-ui/core/styles";
-import InputBase from "@material-ui/core/InputBase";
 import SearchIcon from "@material-ui/icons/Search";
-import Grid from "@material-ui/core/Grid";
 import './style.css'
+import { TextField } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   search: {
-    border: "1px solid black",
     position: "relative",
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: fade(theme.palette.common.white, 0.25)
-    },
-    marginRight: theme.spacing(2),
-    marginInline: "auto",
-    width: "25px",
-    [theme.breakpoints.up("sm")]: {
-      marginLeft: theme.spacing(3),
-      width: "auto"
-    }
+    width: "75%",
+    marginRight: "auto",
+    marginLeft: "auto"
   },
   searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  inputRoot: {
-    color: "inherit"
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create("width"),
-    width: "50%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch"
-    }
+    padding: theme.spacing(0, 1, 0, 0),
+    color: fade(theme.palette.common.black, 0.3)
   }
 }));
 
 export default function Search() {
   const classes = useStyles();
+  const [query, setQuery] = useState("");
+
+  let timeout;
+
+  const handleInputChange = (e) => {
+    clearTimeout(timeout);
+    search(e);
+  };
+
+  const search = (e) => {
+    const currQuery = e.target.value
+    timeout = setTimeout(() => setQuery(currQuery), 1000);
+  };
 
   return (
-    <>
-      {/* Still need to fix the CSS on the search input */}
-      <Grid item xs={6}>
-      <div className={classes.search}>
-        <div className={classes.searchIcon}>
-          <SearchIcon />
-        </div>
-        <InputBase
-          placeholder="Search…"
-          classes={{
-            root: classes.inputRoot,
-            input: classes.inputInput
-          }}
-          inputProps={{ "aria-label": "search"}}
-        />
-      </div>
-      </Grid>
-    </>
+    <div>
+      <TextField
+        className={classes.search}
+        label={query && "Search for movies"}
+        variant="outlined"
+        type="text"
+        placeholder="Search for movies"
+        onChange={handleInputChange}
+        InputProps={{
+          startAdornment: <SearchIcon className={classes.searchIcon} />
+        }}
+      />
+    </div>
   );
 }
