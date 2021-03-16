543cafcaf424778d7abb74c58e9d278d0097c6ef
JES20 <dore.joel.dore@gmail.com>
2021-03-16 19:57:07 -0400

    Fix bug in rendering ratings

--- client/src/components/MovieCard/index.js
+++ client/src/components/MovieCard/index.js
@@ -58,72 +58,71 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
-export default function MovieCard({ movie, updateUsers }) {
+export default function MovieCard(props) {
     const classes = useStyles();
     const location = useLocation();
     const user = useContext(UserContext);
 
-    const [value, setValue] = useState(2);
+    // const [value, setValue] = useState(0);
     // index of value when user hovers
     const [hover, setHover] = useState(-1);
 
     const handleAddMovie = () => {
-        API.addMovie(movie, user.email)
+        API.addMovie(props.movie, user.email)
             .then(({ data }) => console.log("success!", data.nModified, " modified"))
     }
 
     const toggleWatched = (isWatched) => {
-        API.toggleWatched(user.email, movie.title, isWatched)
-            .then(() => updateUsers())
+        API.toggleWatched(user.email, props.movie.title, isWatched)
+            .then(() => props.updateUsers())
     }
 
     const updateRating = (event) => {
-        API.setRating(user.email, movie.title, event.target.value)
-            .then(() => updateUsers())
+        API.setRating(user.email, props.movie.title, event.target.value)
+            .then(() => props.updateUsers())
     }
 
     return (
         <Grid item xs={location.pathname === '/collections' ? 12 : 6}>
             <Card className={classes.root} style={{ marginLeft: "auto", marginRight: "auto" }}>
-                <CardHeader title={movie.title} subheader={"Release Date: " + movie.release_date} />
+                <CardHeader title={props.movie.title} subheader={"Release Date: " + props.movie.release_date} />
                 <CardMedia
                     className={classes.media}
-                    image={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+                    image={`https://image.tmdb.org/t/p/original${props.movie.poster_path}`}
                     title="Movie poster"
                 />
                 <CardContent>
                     <details>
                         <summary>Overview</summary>
-                        {movie.overview}
+                        {props.movie.overview}
                     </details>
                 </CardContent>
                 <CardActions disableSpacing>
                     {
                         location.pathname === '/collections' ? (
                             <>
-                                <IconButton aria-label="add to favorites" onClick={() => toggleWatched(!movie.watched)}>
+                                <IconButton aria-label="add to favorites" onClick={() => toggleWatched(!props.movie.watched)}>
                                     {
-                                        !movie.watched && <VisibilityOffIcon />
+                                        !props.movie.watched && <VisibilityOffIcon />
                                     }
                                     {
-                                        movie.watched && <VisibilityIcon />
+                                        props.movie.watched && <VisibilityIcon />
                                     }
                                 </IconButton>
                                 {
-                                    movie.watched && <div className={classes.rating}>
+                                    props.movie.watched && <div className={classes.rating}>
                                         <Rating
-                                            name="hover-feedback"
-                                            value={value}
+                                            name={props.movie.title}
+                                            value={props.movie.rating}
+                                            defaultValue={0}
                                             precision={0.5}
                                             onChange={(event, newValue) => {
-                                                setValue(newValue);
-                                                // updateRating(event)
+                                                updateRating(event)
                                             }}
                                             onChangeActive={(event, newHover) => {
                                                 setHover(newHover);
                                             }}
                                         />
-                                        {value !== null && <Box ml={2}>{labels[hover !== -1 ? hover : value]}</Box>}
                                     </div>
                                 }
                             </>
