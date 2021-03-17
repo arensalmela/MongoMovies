91c63eacaa7910a90eddd91d9d9b4e3c109d4268
JES20 <dore.joel.dore@gmail.com>
2021-03-16 20:32:32 -0400

    Add conditionally rendered Badge element

--- client/src/components/CollectionCardGrid/index.js
+++ client/src/components/CollectionCardGrid/index.js
@@ -5,6 +5,9 @@ import Grid from "@material-ui/core/Grid";
 import API from "../../utils/API";
 import UserContext from "../../utils/UserContext";
 import MovieCard from "../MovieCard";
+import goldBadge from '../../assets/images/gold-medal-svgrepo-com.svg'
+import silverBadge from '../../assets/images/silver-medal-svgrepo-com.svg'
+import bronzeBadge from '../../assets/images/bronze-medal-svgrepo-com.svg'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,6 +23,12 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: "auto",
     marginRight: "auto",
     boxShadow: ".25rem .25rem 1rem #440000"
+  },
+  badge: {
+    position: "absolute",
+    top: "4rem",
+    left: "50%",
+    transform: "translate(-50%)"
   }
 }));
 
@@ -27,6 +36,7 @@ export default function CollectionCardGrid() {
   const classes = useStyles();
   const user = useContext(UserContext);
   const [userMovies, setUserMovies] = useState([]);
+  const [badge, setBadge] = useState("");
 
   const updateUsers = () => {
     API.getUserProfile(user.googleID)
@@ -38,44 +48,53 @@ export default function CollectionCardGrid() {
     updateUsers()
   }, [])
 
+  useEffect(() => {
+    const totalWatched = userMovies.filter(m => m.watched).length
+    const newBadge = totalWatched > 50 ? goldBadge
+      : totalWatched > 20 ? silverBadge
+        : totalWatched > 5 ? bronzeBadge
+          : ""
+    console.log(totalWatched)
+    setBadge(newBadge)
+  }, [userMovies])
+
   return (
-    <div className={classes.root}>
-      <Grid container>
+    <>
 
-        <Grid item xs={6} id="Unwatched">
-          <h3>Unwatched</h3>
-          <Paper className={classes.paper}>
-            {
-              userMovies.length
-                ? userMovies.filter(m => !m.watched).map(movie => (
-                  <>
-                    <MovieCard movie={movie} key={movie.id} updateUsers={updateUsers} />
-                    <br />
-                    <br />
-                  </>
-                ))
-                : <h2>You haven't added any movies yet!</h2>
-            }
-          </Paper>
-        </Grid>
+      {
+        badge &&
+        <img src={badge} alt="User Badge" width={48} height={48} className={classes.badge} />
+      }
+      <div className={classes.root}>
+        <Grid container>
 
-        <Grid item xs={6} id="Watched">
-          <h3>Watched</h3>
-          <Paper className={classes.paper}>
-            {
-              userMovies.length
-                ? userMovies.filter(m => m.watched).map(movie => (
-                  <>
-                    <MovieCard movie={movie} key={movie.id} updateUsers={updateUsers} />
-                    <br />
-                    <br />
-                  </>
-                ))
-                : <h2>You haven't added any movies yet!</h2>
-            }
-          </Paper>
+          <Grid item xs={6} id="Unwatched">
+            <h3>Unwatched</h3>
+            <Paper className={classes.paper}>
+              {
+                userMovies.length
+                  ? userMovies.map(movie => (
+                    !movie.watched && <MovieCard movie={movie} key={movie.id} updateUsers={updateUsers} />
+                  ))
+                  : <h2>You haven't added any movies yet!</h2>
+              }
+            </Paper>
+          </Grid>
+
+          <Grid item xs={6} id="Watched">
+            <h3>Watched</h3>
+            <Paper className={classes.paper}>
+              {
+                userMovies.length
+                  ? userMovies.map(movie => (
+                    movie.watched && <MovieCard movie={movie} key={movie.id} updateUsers={updateUsers} />
+                  ))
+                  : <h2>You haven't added any movies yet!</h2>
+              }
+            </Paper>
+          </Grid>
         </Grid>
-      </Grid>
-    </div>
+      </div>
+    </>
   );
 }
