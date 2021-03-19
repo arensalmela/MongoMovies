5e80e3e96ebf2e35e1dd7f971a6e415b25a4189f
Aren S <adsalmela@gmail.com>
2021-03-18 21:42:21 -0400

    Quick fix

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,15 +1,13 @@
-const router = require('express').Router();
-const path = require('path')
+const router = require("express").Router();
+const path = require("path");
 //const app = express();
-const userController = require('../../controllers/userController');
+const userController = require("../../controllers/userController");
 
 //adding new user to db
-router.route('/login').post(
-    userController.create
-)
+router.route("/login").post(userController.create);
 
-router.get((req, res) => {
-    res.sendFile(path.join(__dirname, '../../client/build/index.html'))
-})
+router.use((req, res) => {
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+});
 
 module.exports = router;
